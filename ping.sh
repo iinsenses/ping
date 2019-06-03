025d2eed0354d602b28ef0899d1839592f5a9b70
iinsenses <48059083+iinsenses@users.noreply.github.com>
2019-06-04 02:06:27 +0700

    Add files via upload

---
+++ ping.sh
@@ -0,0 +1,85 @@
+#!/system/xbin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f slant -F gay "T00Ls-PING SENSES" | lolcat
+echo "\033[35;1m"
+cal
+date
+echo ""      
+echo "\033[32;1mAuthor : 53N535" 
+echo "\033[35;1mTeam : SENSES CYBER TEAM"
+echo "\033[36;1mIndonesia Security Lite"
+echo "\033[37;1mJawaTengah GalaWi Army"
+echo "\033[33;1mKontak Me :  .........."
+echo "\033[31;1mSupkreb Youtube:"
+echo "\033[33;1mSensesCyberTeam" "\033[32;1m&" "\033[36;1mIndonesia Security Lite"
+echo "\033[34;1m[============================[>"
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "1.> Mempercepat / Menstabilkan Jaringan/sinyal"
+echo "[===============================================]>"
+echo "2.> Mempercepat / menstabilkan jaringan 2"
+
+echo "[===============================================]>"
+echo "0.> keluar"
+echo "[===============================================]>"
+read -p "root@T00Ls-B4J1N64Nv4]~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "53N535" | lolcat
+sleep 1
+echo "\033[32;m====================Mempercepat / menstabilkan sinyal===================="
+sleep 1
+echo "\033[32;1m================================53N535================================"
+sleep 1
+echo ""
+echo ""
+ping 8.8.8.8 
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet "53N535" | lolcat
+sleep 1
+echo "\033[32;m===================Mempercepat / menstabilkan sinyal 2===================="
+sleep 1
+echo "\033[32;1m================================53N535================================"
+sleep 1
+echo ""
+echo ""
+ping -D www.google.com
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mWe Are Family SENSES CYBER TEAM"
+sleep 1
+echo "\033[33;1mWe Are Anonymous"
+sleep 1
+echo "We Are Legion"
+sleep 1
+echo "We Do Not For Give"
+sleep 1
+echo "We Do Not For Get"
+sleep 1
+echo "The Indonesian Anonymous Family"
+sleep 1
+echo "Expect Us"
+sleep 1
+echo "\033[32;1mIncarlah suatu hal mustahil:) Suatu saat Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
+
